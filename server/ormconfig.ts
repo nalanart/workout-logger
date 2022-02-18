bf389173afaae3415d37e1d4a8c220fe0762ce37
nalanart <alantran96@gmail.com>
2022-02-18 18:15:31 -0500

    Create config for connection to pg db

---
+++ server/ormconfig.ts
@@ -0,0 +1,14 @@
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'workout-logger',
+  entities: ['dist/src/**/*.entity{.ts,.js}'],
+  synchronize: true
+}
+
+export default config;
